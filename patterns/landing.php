7e4a2adb34c4499276fc7289678e9347a1f83b90
365Evergreen <55726264+365Evergreen@users.noreply.github.com>
2025-07-31 00:31:54 +1000

    Imported files from live site

---
+++ patterns/landing.php
@@ -0,0 +1,256 @@
+<?php
+/**
+ * Title: landing
+ * Slug: 365-evergreen/landing
+ * Inserter: no
+ */
+?>
+<!-- wp:template-part {"slug":"365-evergreen-header","area":"header"} /-->
+
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/samantha-gades-BlIhVfXbi9s-unsplash-2-1536x1026-1-300x200.webp","dimRatio":50,"customOverlayColor":"#c1c0c2","isUserOverlayColor":false,"focalPoint":{"x":0,"y":1},"minHeight":750,"minHeightUnit":"px","isDark":false,"sizeSlug":"medium","style":{"spacing":{"margin":{"top":"-100px"}}},"layout":{"type":"constrained","contentSize":"100%"}} -->
+<div class="wp-block-cover is-light" style="margin-top:-100px;min-height:750px"><img class="wp-block-cover__image-background  size-medium" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/samantha-gades-BlIhVfXbi9s-unsplash-2-1536x1026-1-300x200.webp" style="object-position:0% 100%" data-object-fit="cover" data-object-position="0% 100%"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#c1c0c2"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"padding":{"right":"50px","left":"50px"}}},"layout":{"type":"constrained","contentSize":"50%","justifyContent":"left"}} -->
+<div class="wp-block-group" style="padding-right:50px;padding-left:50px"><!-- wp:heading {"textAlign":"left","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"large"} -->
+<h1 class="wp-block-heading has-text-align-left has-large-font-size" style="font-style:normal;font-weight:400">We transform your business with Microsoft 365&nbsp;and&nbsp;Power Platform </h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p class="">Australia's trusted Microsoft 365 and Power Platform consultancy. We help businesses unlock productivity, streamline processes, and drive sustainable innovation.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"black","style":{"elements":{"link":{"color":{"text":"var:preset|color|black"}}},"color":{"background":"#ffffff00"},"border":{"width":"1px","color":"#000000","radius":"150px"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-black-color has-text-color has-background has-link-color has-border-color wp-element-button" style="border-color:#000000;border-width:1px;border-radius:150px;background-color:#ffffff00">Find out more...</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:group {"tagName":"main","className":"is-style-default","style":{"spacing":{"padding":{"right":"0px","left":"0px","top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"},"margin":{"top":"-2px","bottom":"-2px"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"black","textColor":"white","fontSize":"small","layout":{"type":"constrained","contentSize":"100%"}} -->
+<main class="is-style-default wp-block-group has-white-color has-black-background-color has-text-color has-background has-link-color has-small-font-size" style="margin-top:-2px;margin-bottom:-2px;padding-top:var(--wp--preset--spacing--30);padding-right:0px;padding-bottom:var(--wp--preset--spacing--30);padding-left:0px"></main>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|80","left":"var:preset|spacing|80","top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}},"layout":{"type":"constrained","contentSize":"80%"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--80)"><!-- wp:columns {"style":{"spacing":{"padding":{"right":"50px","left":"50px"}}}} -->
+<div class="wp-block-columns" style="padding-right:50px;padding-left:50px"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}},"border":{"width":"1px","color":"#9b51e0","radius":"10px"}}} -->
+<div class="wp-block-column has-border-color" style="border-color:#9b51e0;border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"medium"} -->
+<h4 class="wp-block-heading has-medium-font-size" style="font-style:normal;font-weight:400">Modern workplace</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontFamily":"roboto"} -->
+<p class="has-roboto-font-family" style="font-style:normal;font-weight:300">Modern collaboration and streamlined productivity for Aussie teams</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list {"style":{"typography":{"fontStyle":"normal","fontWeight":"300","lineHeight":"1.3","fontSize":"15px"}}} -->
+<ul style="font-size:15px;font-style:normal;font-weight:300;line-height:1.3" class="wp-block-list"><!-- wp:list-item -->
+<li class="">Teams setup &amp; training ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">SharePoint intranet design ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Secure email &amp; migration ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">File organization &amp; access ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Staff onboarding workflows</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:buttons {"style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|20"}}},"layout":{"type":"flex","justifyContent":"right"}} -->
+<div class="wp-block-buttons" style="padding-top:0;padding-bottom:var(--wp--preset--spacing--20)"><!-- wp:button {"textColor":"black","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}},"color":{"background":"#ffffff00"},"elements":{"link":{"color":{"text":"var:preset|color|black"}}},"border":{"width":"1px"}},"borderColor":"black"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-black-color has-text-color has-background has-link-color has-border-color has-black-border-color wp-element-button" href="https://365evergreen.com/modern-intranets/" style="border-width:1px;background-color:#ffffff00;padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20)">Read more...</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}},"border":{"width":"1px","color":"#fcb900","radius":"10px"}}} -->
+<div class="wp-block-column has-border-color" style="border-color:#fcb900;border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"medium","fontFamily":"roboto"} -->
+<h4 class="wp-block-heading has-roboto-font-family has-medium-font-size" style="font-style:normal;font-weight:400">Smart agents</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
+<p style="font-style:normal;font-weight:300" class="">Copilot agents </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list {"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
+<ul style="font-style:normal;font-weight:300" class="wp-block-list"><!-- wp:list-item -->
+<li class="">Bespoke Power Apps ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Automated approvals ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Dashboard visualizations ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Connect your spreadsheets ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Mobile-friendly interfaces</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30","top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}},"border":{"radius":"10px","width":"1px"}},"borderColor":"vivid-green-cyan"} -->
+<div class="wp-block-column has-border-color has-vivid-green-cyan-border-color" style="border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"medium","fontFamily":"roboto"} -->
+<h4 class="wp-block-heading has-roboto-font-family has-medium-font-size" style="font-style:normal;font-weight:400">Process automation</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p class="">Save hours. Automate the boring stuff—emails, updates, approvals</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li class="">Admin process automation ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Scheduled reminders &amp; alerts ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Cross-platform syncing ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Auto-generated reports ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Client onboarding flows</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"style":{"spacing":{"padding":{"right":"50px","left":"50px"}}}} -->
+<div class="wp-block-columns" style="padding-right:50px;padding-left:50px"><!-- wp:column {"style":{"border":{"width":"1px","color":"#abb8c3","radius":"10px"},"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-column has-border-color" style="border-color:#abb8c3;border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
+<h4 class="wp-block-heading" style="font-style:normal;font-weight:300">Business applications</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p class="">“Turn your data into decisions—with clarity and speed.”</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li class="">Financial snapshots ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Team productivity tracking ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Client engagement metrics ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Sales forecasting tools ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Visual KPIs</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}},"border":{"width":"1px","color":"#cf2e2e","radius":"10px"}}} -->
+<div class="wp-block-column has-border-color" style="border-color:#cf2e2e;border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
+<h4 class="wp-block-heading" style="font-style:normal;font-weight:300">Security</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p class="">“Tech doesn’t have to be confusing. We make it work for your goals.”</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li class="">Digital process mapping ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Tailored tech roadmap ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Staff adoption coaching ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Security &amp; compliance advice ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Ongoing support</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}},"border":{"radius":"10px","width":"1px"}},"borderColor":"vivid-cyan-blue"} -->
+<div class="wp-block-column has-border-color has-vivid-cyan-blue-border-color" style="border-width:1px;border-radius:10px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}}} -->
+<h4 class="wp-block-heading" style="font-style:normal;font-weight:300">Support</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p class="">“Everything your business needs—no jargon, just helpful tech.”</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li class="">Cloud storage that fits ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Simple document sharing ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Affordable licenses ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Local, relatable support ✔️</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li class="">Client-ready templates</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Blog"},"className":"nfd-container nfd-p-lg  nfd-wb-blog__blog-9 is-style-nfd-theme-white","style":{"spacing":{"padding":{"top":"0px","bottom":"0px"}}},"layout":{"type":"constrained"},"nfdGroupTheme":""} -->
+<div class="nfd-container nfd-p-lg  nfd-wb-blog__blog-9 is-style-nfd-theme-white wp-block-group" style="padding-top:0px;padding-bottom:0px"><!-- wp:heading -->
+<h2 class="wp-block-heading">Latest posts</h2>
+<!-- /wp:heading -->
+
+<!-- wp:query {"queryId":70,"query":{"perPage":6,"pages":"0","offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":false},"className":"nfd-query-loop-3"} -->
+<div class="nfd-query-loop-3 wp-block-query"><!-- wp:post-template {"style":{"spacing":{"blockGap":"20px"}},"layout":{"type":"grid","columnCount":3}} -->
+<!-- wp:group {"className":"nfd-h-full nfd-rounded-sm nfd-shadow-xs nfd-overflow-hidden is-style-nfd-theme-white","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"border":{"width":"1px"}},"layout":{"type":"constrained"},"nfdGroupTheme":"white","nfdGroupEffect":""} -->
+<div class="nfd-h-full nfd-rounded-sm nfd-shadow-xs nfd-overflow-hidden is-style-nfd-theme-white wp-block-group" style="border-width:1px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|30","left":"var:preset|spacing|20","right":"var:preset|spacing|30"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--20)"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"customOverlayColor":"#d9d5e8","isUserOverlayColor":false,"minHeight":221,"minHeightUnit":"px","customGradient":"linear-gradient(180deg,rgba(0,0,0,0.06) 80%,rgb(255,255,255) 80%)","contentPosition":"top left","isDark":false,"className":"nfd-stretch-cover-child","style":{"spacing":{"padding":{"top":"20px","bottom":"20px","left":"20px","right":"20px"}}},"layout":{"type":"constrained"}} -->
+<div class="nfd-stretch-cover-child wp-block-cover is-light has-custom-content-position is-position-top-left" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px;min-height:221px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim has-background-gradient" style="background-color:#d9d5e8;background:linear-gradient(180deg,rgba(0,0,0,0.06) 80%,rgb(255,255,255) 80%)"></span><div class="wp-block-cover__inner-container"><!-- wp:post-title {"level":3,"isLink":true,"className":"nfd-text-md nfd-text-contrast","style":{"typography":{"fontStyle":"normal","fontWeight":"500"}}} /-->
+
+<!-- wp:post-author {"textAlign":"left","style":{"layout":{"selfStretch":"fit","flexSize":null}},"textColor":"secondary-foreground"} /-->
+
+<!-- wp:post-terms {"term":"category","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|black"}}}}}} /--></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
+<!-- /wp:query --></div>
+<!-- /wp:group -->
